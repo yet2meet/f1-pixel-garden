4efae3f1f9880e8d4b2687ccd03da48fea063f05
yet2meet <hyq20222004@gmail.com>
2026-06-03 14:09:21 +0800

    Fix mobile UI scrolling and side driver names

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "f1-pixel-pwa-v16";
+const CACHE_NAME = "f1-pixel-pwa-v19";
 
 const drivers = ["verstappen", "leclerc", "hamilton", "norris", "piastri", "russell", "antonelli", "alonso"];
 const expressions = ["neutral", "tap", "anticipate", "eat", "satisfied", "celebrate", "tired", "depleted"];
